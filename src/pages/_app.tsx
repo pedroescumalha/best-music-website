035b7f55d183ace26eee1ba8a824aec95222e1b2
Pedro Simões <pedrosimoes93@gmail.com>
2023-01-24 17:39:56 +0000

    created footer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import NavBar from "@/components/NavBar";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
@@ -16,6 +17,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <main>
         <Component {...pageProps} />
       </main>
+      <Footer />
     </>
   );
 }
