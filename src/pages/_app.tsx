789c159ed636a56fbe4fbfa103c4583f16abbb9e
pedroescumalha <63684493+pedroescumalha@users.noreply.github.com>
2023-01-24 17:07:23 +0000

    Created nav bar (#5)
    * animate background
    
    * added navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,7 +11,19 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="h-screen">
+      <header className="flex justify-center pt-5">
+        <nav className="w-11/12 bg-white px-6 py-3 flex items-center justify-between shadow-sm">
+          <div>
+            <h1 className="text-4xl font-extrabold tracking-wide">Escumalha</h1>
+          </div>
+          <div className="text-end">
+            <p>bio</p>
+            <p>music</p>
+            <p>contacts</p>
+          </div>
+        </nav>
+      </header>
+      <main>
         <Component {...pageProps} />
       </main>
     </>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ import Head from "next/head";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-4xl font-sans font-extrabold tracking-wide">Escumalha</h1>
     </div>
   );
 }
