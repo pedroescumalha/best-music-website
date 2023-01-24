f01d48edb1c8b620f320e2028d35e1609b4880d8
Pedro Simões <pedrosimoes93@gmail.com>
2023-01-24 14:34:50 +0000

    add inline background gradient

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,16 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body style={{
+        background: `
+          linear-gradient(115.58deg, var(--color-green-400) 0%, var(--color-blue-900) 100.22%),
+          radial-gradient(92.72% 100% at 50% 0%, var(--color-green-100) 0%, var(--color-red-900) 100%),
+          radial-gradient(92.72% 100% at 50% 0%, var(--color-yellow-200) 0%, var(--color-red-900) 100%),
+          radial-gradient(109.21% 213.32% at 100% 0%, var(--color-orange-500) 0%, var(--color-sky-400) 100%),
+          linear-gradient(127.43deg, var(--color-red-600) 0%, var(--color-violet-600) 100%)
+        `,
+        backgroundBlendMode: "lighten, overlay, lighten, screen, normal"
+      }}>
         <Main />
         <NextScript />
       </body>
