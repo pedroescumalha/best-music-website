3281b504bcab2e2c03f3caf0201d2517b448c94b
pedroescumalha <63684493+pedroescumalha@users.noreply.github.com>
2023-01-24 13:46:49 +0000

    Added custom font (#3)
    * added lint rules
    
    * fix lint
    
    * Update tailwind.config.js
    
    * added custom font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>ESCUMALHA</title>
+        <meta name="description" content="ESCUMALHA" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="h-screen">
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
