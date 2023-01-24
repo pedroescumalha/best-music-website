5d1dfd8469a0b81e360e4437316e77a38defcb8c
Pedro Simões <pedrosimoes93@gmail.com>
2023-01-24 17:15:53 +0000

    moved navbar to its own component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import NavBar from "@/components/NavBar";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
@@ -11,18 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header className="flex justify-center pt-5">
-        <nav className="w-11/12 bg-white px-6 py-3 flex items-center justify-between shadow-sm">
-          <div>
-            <h1 className="text-4xl font-extrabold tracking-wide">Escumalha</h1>
-          </div>
-          <div className="text-end">
-            <p>bio</p>
-            <p>music</p>
-            <p>contacts</p>
-          </div>
-        </nav>
-      </header>
+      <NavBar />
       <main>
         <Component {...pageProps} />
       </main>
